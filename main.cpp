6f6fd0169449a908d1eea386f3c72ce298dfe9a7
Serenity0204 <yuhenglin0204@gmail.com>
2022-10-28 12:59:48 -0700

    can successfully load the sprite of different cards

--- main.cpp
+++ main.cpp
@@ -2,32 +2,82 @@
 #include <SFML/Graphics.hpp>
 using namespace std;
 
+const string PATH = "../assets/";
+const string CLUBS = "_of_clubs.png";
+const string DIAMONDS = "_of_diamonds.png";
+const string HEARTS = "_of_hearts.png";
+const string SPADES = "_of_SPADES.png";
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML Test!");
-    // sf::RenderWindow window(sf::VideoMode(200, 200), "There's a cat!");
-    // sf::CircleShape shape(100.f);
-    // shape.setFillColor(sf::Color::Green);
-    // sf::Texture texture;
-    // if(texture.loadFromFile("../assets/cat1.png")) cout << "load success" << endl;
+    sf::RenderWindow window(sf::VideoMode(1500, 1000), "Black Jack");
+    vector<vector<sf::Texture>> cards_holder(4);
+    vector<vector<sf::Sprite>> cards(4);
+    vector<sf::Texture> bets;
     
-    // sf::Sprite sprite;
-    // sprite.setTexture(texture, true);
 
-    sf::Font font;
-    font.loadFromFile("../assets/arial.ttf");
-    sf::Text text;
-    // select the font
-    text.setFont(font); // font is a sf::Font
-    // set the string to display
-    text.setString("Hello world");
-    // set the character size
-    text.setCharacterSize(24); // in pixels, not points!
-    // set the color
-    text.setFillColor(sf::Color::Red);
-    // set the text style
-    text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+    // creating sprites
+    for(int i = 0; i < 4; ++i)
+    {
+        sf::Texture t;
+        for(int j = 0; j < 13; ++j)
+        {
+            cards_holder[i].push_back(t);    
+        }
+    }
+
+
+    // loading cards
+    for(int i = 0; i < 13; ++i)
+    {
+        string temp = "";
+
+        // get the substring path
+        switch (i)
+        {
+        case 0:
+            temp += "ace";
+            break;
+        case 10:
+            temp += "jack";
+            break;
+        case 11:
+            temp += "queen";
+            break;
+        case 12:
+            temp += "king";
+            break;
+        default:
+            temp += to_string(i + 1);
+            break;
+        }
+
+        string clubs = PATH + temp + CLUBS;
+        cards_holder[0][i].loadFromFile(clubs);
+        
+        string diamonds = PATH + temp + DIAMONDS;
+        cards_holder[1][i].loadFromFile(diamonds);
+
+        string hearts = PATH + temp + HEARTS;
+        cards_holder[2][i].loadFromFile(hearts);
+
+        string spades = PATH + temp + SPADES;
+        cards_holder[3][i].loadFromFile(spades);
+    }
+
+
+    // creating sprites
+    for(int i = 0; i < 4; ++i)
+    {
+        for(int j = 0; j < 13; ++j)
+        {
+            sf::Sprite sprite(cards_holder[i][j]);
+            cards[i].push_back(sprite);
+        }
+    }
+
+
+
 
 
     while (window.isOpen())
@@ -40,10 +90,7 @@ int main()
         }
 
         window.clear();
-
-        window.draw(text);
-        //window.draw(sprite);
-        //window.draw(shape);
+        window.draw(cards[3][11]);
         window.display();
     }
 
